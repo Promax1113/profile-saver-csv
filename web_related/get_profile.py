31ce47be373a0147f1da1fe438c200829585bead
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2023-08-14 11:32:35 +0200

    init

---
+++ web_related/get_profile.py
@@ -0,0 +1,40 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+
+def get_profile(__username, webpage):
+    chrome = Options()
+    # chrome.add_argument("--headless")
+    chrome.add_argument("--disable-gpu")
+    driver = webdriver.Chrome(options=chrome)
+    driver.get(f"https://www.twitter.com/{__username}")
+
+    time.sleep(5)
+
+    display_name = driver.find_element(By.XPATH,
+                                       "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div[1]/div/div[1]/div/div/span/span[1]").text
+
+    username = driver.find_element((By.XPATH,
+                                    "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div[1]/div/div[2]/div/div/div/span")).text
+    try:
+        about = driver.find_element(By.XPATH,
+                                    "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[3]/div/div/span").text
+    except:
+        about = "N/A"
+    try:
+        location = driver.find_element(By.XPATH,
+                                       "div#react-root > div > div > div:nth-of-type(2) > main > div > div > div > div > div > div:nth-of-type(3) > div > div > div > div > div:nth-of-type(4) > div > span > span > span").text
+    except:
+        location = "N/A"
+
+    account_date = driver.find_element(By.XPATH,
+                                       "div#react-root > div > div > div:nth-of-type(2) > main > div > div > div > div > div > div:nth-of-type(3) > div > div > div > div > div:nth-of-type(4) > div > span:nth-of-type(2) > span").text
+
+    return {'display_name': display_name, 'username': username, 'about': about, 'location': location,
+            'creation_date': account_date}
+
+
+print(get_profile("promax1113", "baller"))
