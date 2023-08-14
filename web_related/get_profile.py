df2bd5ef782b579113f6950b209fa0b42768dce7
Promax1113 <67706765+Promax1113@users.noreply.github.com>
2023-08-14 12:45:52 +0200

    Add data fetching

--- web_related/get_profile.py
+++ web_related/get_profile.py
@@ -6,9 +6,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
 
-def get_profile(__username, webpage):
+def get_profile(__username):
     chrome = Options()
-    # chrome.add_argument("--headless")
+    chrome.add_argument("--headless")
     chrome.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=chrome)
     driver.get(f"https://www.twitter.com/{__username}")
@@ -31,14 +31,14 @@ def get_profile(__username, webpage):
     except:
         location = "N/A"
 
-    account_date = driver.find_element(By.XPATH,
+    account_year = driver.find_element(By.XPATH,
                                        "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div/div/div[4]/div/span[2]/span").text
 
-    account_date = account_date.split(" ")
-    account_date = account_date[1] + account_date[2]
-    account_date = datetime.datetime.strptime(account_date, "%m%Y")
+    account_year = account_year.split(" ")
+    account_year = account_year[2]
+
     return {'display_name': display_name, 'username': username, 'about': about, 'location': location,
-            'creation_date': account_date}
+            'creation_year': account_year}
 
 
-print(get_profile("elonmusk", "baller"))
+print(get_profile("elonmusk"))
